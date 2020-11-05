822e60735743c9639a786fb86ba2212112a4140f
Akira <akira.matsuda@me.com>
2020-11-05 18:11:00 +0900

    Update example

--- Example/TableViewContent/ViewController.swift
+++ Example/TableViewContent/ViewController.swift
@@ -17,13 +17,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        title = "Example"
         let dataSource = ContentDataSource {
             Section {
+                DefaultTableViewCell(title: "title")
                 DefaultTableViewCell(title: "title", style: .subtitle)
                     .detailText("subtitle")
                 DefaultTableViewCell(title: "title", style: .value1)
                     .detailText("value1")
-                    .selectionStyle(.none)
                 DefaultTableViewCell(title: "title", style: .value2)
                     .accessoryType(.disclosureIndicator)
                     .detailText("value2")
@@ -34,7 +35,6 @@ class ViewController: UIViewController {
             }
             Section {
                 SwitchCell(title: "Switch")
-                    .selectionStyle(.none)
                 SwitchCell(title: "Switch2", isOn: true)
                     .toggled { (isOn) in
                         print("\(isOn)")
@@ -43,16 +43,13 @@ class ViewController: UIViewController {
                     }
             }
             Section {
-                DefaultTableViewCell(title: "title")
-            }
-            Section {
                 CustomCell {
                     print("button pressed")
                 }
             }
             Section()
                 .header(.title("header3"))
-                .contents { (section) in
+                .contents { section in
                     for i in 0...5 {
                         section.append(DefaultTableViewCell(title: "\(i)"))
                     }
@@ -60,7 +57,7 @@ class ViewController: UIViewController {
                 .footer(.title("footer3"))
             Section()
                 .header(.nib("custom header", UINib(nibName: "CustomHeaderView", bundle: nil)))
-                .contents { (section) in
+                .contents { section in
                     for i in 0...5 {
                         section.append(DefaultTableViewCell(title: "\(i)"))
                     }
@@ -70,10 +67,6 @@ class ViewController: UIViewController {
         delegate = ContentDelegate(dataSource: dataSource)
         tableView.delegate = delegate
         tableView.dataSource = dataSource
-        tableView.estimatedSectionHeaderHeight = 44
-        tableView.sectionHeaderHeight = UITableView.automaticDimension
-        tableView.estimatedSectionFooterHeight = 44
-        tableView.sectionFooterHeight = UITableView.automaticDimension
     }
 
     override func didReceiveMemoryWarning() {

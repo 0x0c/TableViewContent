24d2c4c2d9500ad7785d9ac5ff002721bd96be2d
Akira <akira.matsuda@me.com>
2021-10-12 23:14:11 +0900

    Use @resultBuilder

--- TableViewContent/Classes/Section/Section.swift
+++ TableViewContent/Classes/Section/Section.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-@_functionBuilder
+@resultBuilder
 public enum CellBuilder {
     public static func buildBlock(_ items: RowRepresentation...) -> [RowRepresentation] {
         items
